392b30bec3fb46d5864ddb6dbd216dd3063ec692
s1545151561 <141800803+s1545151561@users.noreply.github.com>
2024-05-07 17:24:23 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -74,9 +74,6 @@ git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
 # MosDNS
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
-# Alist
-git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
-
 # DDNS.to
 git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-app-ddnsto
 git_sparse_clone master https://github.com/linkease/nas-packages network/services/ddnsto
